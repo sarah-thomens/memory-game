cbc833ca0da56f37b069d780bb1a3b3cb6f70709
Sarah Marie Thomens <sarah.thomens@gmail.com>
2018-07-09 14:13:41 -0400

    Adding all cards to array.

--- js/app.js
+++ js/app.js
@@ -1,7 +1,8 @@
-/*
- * Create a list that holds all of your cards
- */
-
+/*==A list that holds all memory cards======================================================================*/
+const memoryCards = [ "fa fa-diamond", "fa fa-paper-plane-o", "fa fa-anchor", "fa fa-bolt", "fa fa-cube",
+                      "fa fa-anchor", "fa fa-leaf", "fa fa-bicycle", "fa fa-diamond", "fa fa-bomb",
+                      "fa fa-leaf", "fa fa-bomb", "fa fa-bolt", "fa fa-bicycle", "fa fa-paper-plane-o",
+                      "fa fa-cube" ];
 
 /*
  * Display the cards on the page
